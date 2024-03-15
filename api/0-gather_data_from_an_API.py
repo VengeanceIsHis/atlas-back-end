a95c3f604fa2bf6d8f9d8efc8a61edad67db94a5
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-03-14 20:35:37 -0500

    there

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -5,25 +5,27 @@ import sys
 
 
 if __name__ == "__main__":
-	url = "https://jsonplaceholder.typicode.com"
+    url = "https://jsonplaceholder.typicode.com"
 
-	employee_id = sys.argv[1]
+    employee_id = sys.argv[1]
 
-	response = requests.get(url + "users/{}".format(employee_id))
+    response = requests.get(url + "users/{}".format(employee_id))
 
-	user = response.json()
+    user = response.json()
 
-	param = {"user.id": employee_id}
+    param = {"user.id": employee_id}
 
-	todos = requests.get(url + "todos", params=param)
+    todos = requests.get(url + "todos", params=param)
 
-	todo = todos.json()
+    todo = todos.json()
 
-	complete = []
+    complete = []
 
-	for todo in todos:
-		if todo.get("completed") is True:
-			complete.append(todo.get("title"))
+    for todo in todos:
+        if todo.get("completed") is True:
+            complete.append(todo.get("title"))
 
-	print("Employee {} is done with tasks".format(user.get("name")))
-	print("{}/{}".format(len(complete), len(todos)))
+    print("Employee {} is done with tasks".format(user.get("name")))
+    print("{}/{}".format(len(complete), len(todos)))
+
+    for completion in complete:
