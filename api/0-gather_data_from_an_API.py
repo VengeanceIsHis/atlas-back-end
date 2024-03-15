1f288ed15350606f1478cd788c2ddfac6a525fe8
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-03-14 20:17:49 -0500

    test

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,39 +1,12 @@
 #!/usr/bin/python3
 """New module based around handlings requests to url"""
 import requests
+import sys
 
 
-def request(employee_id):
-    """function that defines url and sends request to said url"""
-    base_url = "https://jsonplaceholder.typicode.com"
-    todo_url = f"{base_url}/todos?userId={employee_id}"
+if __name__ == "__main__":
+ url = "https://jsonplaceholder.typicode.com"
 
-    try:
-        response = requests.get(todo_url)
-        todos = response.json()
+ employee_id = sys.argv[1]
 
-        if not todos:
-            print(f"No TODO list found for employee with ID {employee_id}")
-            return
-
-        # Get employee name
-        user_url = f"{base_url}/users/{employee_id}"
-        user_response = requests.get(user_url)
-        employee_name = user_response.json()['name']
-
-        completed_tasks = []
-        for todo in todos:
-            if todo['completed']:
-                completed_tasks.append(todo['title'])
-
-        total_tasks = len(todos)
-        num_completed_tasks = len(completed_tasks)
-
-        output = f"Employee {employee_name} is done with tasks "
-        output += f"({num_completed_tasks}/{total_tasks}):\n{employee_name}:\n"
-        for task_title in completed_tasks:
-            output += f"\t{task_title}\n"
-        return output
-
-    except requests.exceptions.RequestException as e:
-        return f"Error fetching TODO list: {e}"
+response = requests.get(url + "users/{}".format(employee_id))
