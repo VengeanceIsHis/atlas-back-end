3d80feeb54cd997048fa034af51ab6e501060354
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-03-15 13:31:57 -0500

    test

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -7,16 +7,15 @@ import sys
 def new_data_from_api(employee_id):
     url = "https://jsonplaceholder.typicode.com/"
 
-    employee_id = sys.argv[1]
     user = requests.get(url + "users/{}".format(employee_id)).json()
+    todos = requests.get(url + "todos", params={"userId": employee_id}).json()
 
-    params = {"userId": employee_id}
-    todos = requests.get(url + "todos", params).json()
-
-    completed = [t.get("title") for t in todos if t.get("completed") is True]
+    completed = [t.get("title") for t in todos if t.get("completed")]
 
     print("Employee {} is done with tasks({}/{}):".format(
         user.get("name"), len(completed), len(todos)))
 
-    [print("\t {}".format(complete)) for complete in completed]
-    return (employee_id)
+    for complete in completed:
+        print("\t{}".format(complete))
+
+    return employee_id
