1f6fa2c9af3cd541025eb3e17e2d7df420e81f10
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-03-16 09:48:46 -0500

    this is tough

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -11,6 +11,7 @@ def get_employee_todo(employee_id):
     # Get the employee information using the provided employee ID
     employee_name = requests.get("name")
     user = requests.get(url + "users/{}".format(employee_id)).json()
+    employee = user['name']
 
     # Get the to-do list for the employee using the provided employee ID
     params = {"userId": employee_id}
@@ -21,7 +22,7 @@ def get_employee_todo(employee_id):
 
     # Print the employee's name and the number of completed tasks
     print("Employee {} is done with tasks({}/{}):".format(
-        user.get("name"), len(completed), len(todos)))
+        "employee"), len(completed), len(todos)))
 
     # Print the completed tasks one by one with indentation
     [print("\t {}".format(complete)) for complete in completed]
