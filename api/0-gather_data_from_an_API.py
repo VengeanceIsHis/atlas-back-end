c0d751eb0a677dcd35fbc012ff22372fb3ae82a9
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-03-15 23:03:39 -0500

    test

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,32 +1,26 @@
 #!/usr/bin/python3
-""" Module for retrieving employee TODO list information using an API """
-
 import requests
 import sys
 
 
-def get_employee_todo(employee_id):
-    """ function returns employee to do list information """
-    base_url = "https://jsonplaceholder.typicode.com"
-    employee_url = "{}/users/{}".format(base_url, employee_id)
-    todo_url = "{}/todos".format(base_url)
-
-    employee_data = requests.get(employee_url).json()
-    specific_employee = employee_data['name']
-    todo_list = requests.get(todo_url, params={"userId": employee_id}).json()
-
-    complete_todos = []
-    total_todos = 0
-    for todo in todo_list:
-        total_todos += 1
-        if todo["completed"] is True:
-            complete_todos.append(todo["title"])
-
-    print("Employee {} is done with tasks({}/{}):".format(
-        specific_employee, len(complete_todos), total_todos))
-    for task_title in complete_todos:
-        print("\t {}".format(task_title))
-
-
 if __name__ == "__main__":
-    get_employee_todo(int(sys.argv[1]))
+    # Base URL for the JSONPlaceholder API
+    url = "https://jsonplaceholder.typicode.com/"
+
+    # Get the employee information using the provided employee ID
+    employee_id = sys.argv[1]
+    user = requests.get(url + "users/{}".format(employee_id)).json()
+
+    # Get the to-do list for the employee using the provided employee ID
+    params = {"userId": employee_id}
+    todos = requests.get(url + "todos", params).json()
+
+    # Filter completed tasks and count them
+    completed = [t.get("title") for t in todos if t.get("completed") is True]
+
+    # Print the employee's name and the number of completed tasks
+    print("Employee {} is done with tasks({}/{}):".format(
+        user.get("name"), len(completed), len(todos)))
+
+    # Print the completed tasks one by one with indentation
+    [print("\t {}".format(complete)) for complete in completed]
